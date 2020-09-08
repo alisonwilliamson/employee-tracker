89a4d873e1ab3e269dc43116f04758bc00904271
Allie <williamson.alisonj@gmail.com>
2020-09-08 01:04:01 -0400

    viewDepartments & viewRoles functions

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const consoleTable = require("console.table")
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table')
 
 // creates connection to sql database
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: 'localhost',
     port: 3306,
-    user: "root",
-    password: "maggiemay233",
-    database: "employees_db"
+    user: 'root',
+    password: 'maggiemay233',
+    database: 'employees_db'
 })
 
 // connects to sql server and sql database
@@ -21,102 +21,112 @@ connection.connect(function(err){
 function options() {
     inquirer
     .prompt({
-        name: "action",
-        type: "list",
-        message: "Welcome to our employee database! What would you like to do?",
+        name: 'action',
+        type: 'list',
+        message: 'Welcome to our employee database! What would you like to do?',
         choices: [
-                "View all employees",
-                "View all departments",
-                "View all roles",
-                "Add an employee",
-                "Add a department",
-                "Add a role",
-                "Update employee role",
-                "Delete an employee",
-                "EXIT"
+                'View all employees',
+                'View all departments',
+                'View all roles',
+                'Add an employee',
+                'Add a department',
+                'Add a role',
+                'Update employee role',
+                'Delete an employee',
+                'EXIT'
         ]
     }).then(function (answer) {
         switch (answer.action) {
-            case "View all employees":
+            case 'View all employees':
                 viewEmployees();
                 break;
-            case "View all departments":
+            case 'View all departments':
                 viewDepartments();
                 break;
-            case "View all roles":
+            case 'View all roles':
                 viewRoles();
                 break;
-            case "Add an employee":
+            case 'Add an employee':
                 addEmployee();
                 break;
-            case "Add a department":
+            case 'Add a department':
                 addDepartment();
                 break;
-            case "Add a role":
+            case 'Add a role':
                 addRole();
                 break;
-            case "Update employee role":
+            case 'Update employee role':
                 updateRole();
                 break;
-            case "Delete an employee":
+            case 'Delete an employee':
                 deleteEmployee();
                 break;
-            case "EXIT": 
+            case 'EXIT': 
                 exitApp();
                 break;
             default:
                 break;
         }
     })
-}
+};
 
 // view all employees in the database
 function viewEmployees() {
-    var query = "SELECT * FROM employee";
+    var query = 'SELECT * FROM employee';
     connection.query(query, function(err, res) {
         if (err) throw err;
-        console.log(res.length + " employees found!");
+        console.log(res.length + ' employees found!');
         console.table('All Employees:', res); 
-        exitApp();
+        options();
     })
-}
+};
 
 // view all departments in the database
 function viewDepartments() {
-   
-}
+    var query = 'SELECT * FROM department';
+    connection.query(query, function(err, res) {
+        if(err)throw err;
+        console.table('All Departments:', res);
+        options();
+    })
+};
 
 // view all roles in the database
 function viewRoles() {
-  
-}
+    var query = 'SELECT * FROM role';
+    connection.query(query, function(err, res){
+        if (err) throw err;
+        console.table('All roles:', res);
+        options();
+    })
+};
 
 // add an employee to the database
 function addEmployee() {
 
-}
+};
 
 // add a department to the database
 function addDepartment() {
 
-}
+};
 
 // add a role to the database
 function addRole() {
 
-}
+};
 
 // update a role in the database
 function updateRole() {
 
-}
+};
 
 //  delete an employee
 function deleteEmployee() {
 
-}
+};
 
 // exit the app
 function exitApp() {
-
-}
+    connection.end();
+};
