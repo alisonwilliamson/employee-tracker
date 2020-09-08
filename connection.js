167f4fb11bcdd59bc59163b7232371da354884ed
Allie <williamson.alisonj@gmail.com>
2020-09-07 21:33:02 -0400

    create db and connection files

---
+++ connection.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+require('console.table');
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employees_db"
+  });
+  
+  // Make connection.
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  // Export connection for our ORM to use.
+  module.exports = connection;
+  
---
+++ schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+-- Create the table for department
+CREATE TABLE department (
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Create the table for roles
+CREATE TABLE roles (
+    id int NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(9,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+-- Create the table for employees
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
+
+
